c98991f16e24e8c3ef2f340b7b5b79eb4244f21d
wsydney76 <wsydney76@users.noreply.github.com>
2022-12-09 11:29:12 +0000

    deploy: 8ce303c7af647c97f18d42aaa2f36214454e091e

---
+++ assets/config_plugin-config.md.01cf1d17.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as t,a as s}from"./app.e8f0dc17.js";const h=JSON.parse('{"title":"Plugin config","description":"","frontmatter":{},"headers":[],"relativePath":"config/plugin-config.md"}'),l={name:"config/plugin-config.md"},o=s("",6),n=[o];function i(r,p,c,d,u,g){return t(),a("div",null,n)}const y=e(l,[["render",i]]);export{h as __pageData,y as default};
