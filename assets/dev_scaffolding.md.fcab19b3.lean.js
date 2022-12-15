76c9b117faee761c5512afa0e0a3e8de41eb9a1d
wsydney76 <wsydney76@users.noreply.github.com>
2022-12-15 15:23:26 +0000

    deploy: 5315322254898ed20caa9fd49efb2f5f40a86a86

--- assets/dev_scaffolding.md.fcab19b3.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o as e,a as n}from"./app.b2454dbc.js";const u=JSON.parse('{"title":"Scaffolding","description":"","frontmatter":{},"headers":[],"relativePath":"dev/scaffolding.md"}'),t={name:"dev/scaffolding.md"},c=n("",8),o=[c];function p(l,r,i,d,f,v){return e(),a("div",null,o)}const m=s(t,[["render",p]]);export{u as __pageData,m as default};
